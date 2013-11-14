f2d847ccc870236ae326f54a9d6b0b1ff62d7ae1
Lindsay <lindsay.nauman@gmail.com>
2013-11-14 15:48:13 -0500

    initial commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoomieFinder::Application.config.secret_key_base = 'a7665efa8f1edb5c061a9e1faf11c2de2578c88b3fda0364024ef977881707f19ba83195f87ead129dc5f5aedc1dd0e54016fcb6a241de8656adac6817d644fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoomieFinder::Application.config.session_store :cookie_store, key: '_roomie_finder_session'
