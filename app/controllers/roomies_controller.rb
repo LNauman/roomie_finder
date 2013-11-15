46cf35293329bfea5cd610f26e31b8f97ee119e7
Lindsay <lindsay.nauman@gmail.com>
2013-11-14 19:56:25 -0500

    search results view page

--- app/controllers/roomies_controller.rb
+++ app/controllers/roomies_controller.rb
@@ -1,12 +1,13 @@
 class RoomiesController < ApplicationController
 
-def index
-  @roomies = Roomie.all
-end
+  def index
+    @roomies = Roomie.all
+  end
 
-def search
-  @query = Roomie.where(desired_rent: params[:q].to_i)
-  puts @query
-end
+
+
+  def search
+    @query = Roomie.where(desired_rent: params[:q].to_i)
+  end
 
 end
